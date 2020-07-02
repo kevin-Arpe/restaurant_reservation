23b176dc2595b674deda84b6928ae3c4b98a92aa
kevin_Lee <arpeggiogio@gmail.com>
2020-07-02 19:35:29 +0900

    Project Start, reservation page with MySQL DB

---
+++ app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const url = require('url');
+const topic = require('./lib/topic');
+const db = require('./lib/db');
+
+db.connect();
+
+const app = http.createServer((request, response) => {
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            topic.home(request, response);
+        }
+        else {
+            response.writeHead(302, {Location: '/'});
+            response.end();
+        }
+    }
+    else if (pathname === '/reserving') {
+        topic.reserving(request, response);
+    }
+});
+
+app.listen(3000);
